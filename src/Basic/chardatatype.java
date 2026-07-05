6f26a2583285f46bc4dfd4752ec9ac28fa8fcc76
vijayyadav2097 <vijayyadav186953@gmail.com>
2026-07-05 10:45:43 +0530

    new update

--- src/Basic/chardatatype.java
+++ src/Basic/chardatatype.java
@@ -3,12 +3,12 @@ package Basic;
 public class chardatatype {
     public static  void main(String args[]){
         char vijay = 'f';
-        System.out.println(vijay);
-        char ajay = 's';
-        System.out.println(ajay);
-        char vishal = 'b';
-        System.out.println(ajay);
-        System.out.println("Github ");
+//        System.out.println(vijay);
+//        char ajay = 's';
+//        System.out.println(ajay);
+//        char vishal = 'b';
+//        System.out.println(ajay);
+//        System.out.println("Github ");
 
     }
 
--- src/If_else_condition/terenaryOperator.java
+++ src/If_else_condition/terenaryOperator.java
@@ -1,4 +1,15 @@
 package If_else_condition;
-
+import java.util.Scanner;
 public class terenaryOperator {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your  number");
+        int n = sc.nextInt();
+//        if(n%2==0){
+//            System.out.println("EVEN");
+//       }else{
+//        System.out.println("ODD");
+        System.out.println((n%2==0) ? "EVEN":"ODD");
+    }
+
 }
