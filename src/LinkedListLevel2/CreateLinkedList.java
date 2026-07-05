b6e29f4a671e060faf77e038869662f2691e378a
vijayyadav2097 <vijayyadav186953@gmail.com>
2026-07-05 15:26:43 +0530

    Added remaining java files.

--- src/LinkedListLevel2/CreateLinkedList.java
+++ src/LinkedListLevel2/CreateLinkedList.java
@@ -1,4 +1,53 @@
 package LinkedListLevel2;
+class Node {
+    int value;
+    Node next;
 
-public class CreateLinkedList {
+    Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
 }
+
+class LinkedList {
+    Node head;
+    Node tail;
+
+    // Add node at tail
+    void addNodeAtTail(int value) {
+        Node temp = new Node(value);
+
+        if (head == null) {
+            head = tail = temp;
+            return;
+        }
+
+        tail.next = temp;
+        tail = temp;
+    }
+
+    // Print Linked List
+    void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+}
+public class CreateLinkedList{
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addNodeAtTail(10);
+        ll.addNodeAtTail(20);
+        ll.addNodeAtTail(30);
+        ll.addNodeAtTail(40);
+
+        ll.display();
+    }
+}
+
